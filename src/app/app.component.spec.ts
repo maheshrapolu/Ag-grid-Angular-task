a06eeafc2da147531efbf9c37f2243537eeb1f50
Mahesh Rapolu <mr19394@corp.ojas-it.com>
2020-10-14 12:57:15 +0530

    curd urls

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ag-grid'`, () => {
+  it(`should have as title 'Curdpoc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ag-grid');
+    expect(app.title).toEqual('Curdpoc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ag-grid app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Curdpoc app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgGridModule } from 'ag-grid-angular';
+import { ReactiveFormsModule, FormsModule }    from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {NgxPaginationModule} from 'ngx-pagination'; 
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AgGridModule.withComponents([]),
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule
   ],
-  
   providers: [],
   bootstrap: [AppComponent]
 })
